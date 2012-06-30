57d35e867a21af9b69844f7a0c6d30f65e582643
Tom Barker <tomjbarker@gmail.com>
2012-06-29 23:34:17 -0400

    added window.performance.timing derived properties

--- perfLogger.js
+++ perfLogger.js
@@ -1,7 +1,30 @@
 var perfLogger = function(){
 	var serverLogURL = "savePerfData.php",
-		loggerPool = [];
+		loggerPool = [],
+		_pTime = Date.now() - performance.timing.navigationStart || 0,
+		_redirTime = performance.timing.redirectEnd - performance.timing.redirectStart || 0,
+		_cacheTime = performance.timing.domainLookupStart - performance.timing.fetchStart || 0,
+		_dnsTime = performance.timing.domainLookupEnd - performance.timing.domainLookupStart || 0,
+		_tcpTime = performance.timing.connectEnd - performance.timing.connectStart || 0,
+		_roundtripTime = performance.timing.responseEnd - performance.timing.connectStart || 0,
+		_renderTime = Date.now() - performance.timing.domLoading || 0;
 		
+		function TestResults(){};
+		TestResults.prototype.perceivedTime = _pTime;
+		TestResults.prototype.redirectTime = _redirTime;
+		TestResults.prototype.cacheTime = _cacheTime;
+		TestResults.prototype.dnsLookupTime = _dnsTime;
+		TestResults.prototype.tcpConnectionTime = _tcpTime;
+		TestResults.prototype.roundTripTime = _roundtripTime;
+		TestResults.prototype.pageRenderTime = _renderTime;
+		
+		function jsonConcat(object1, object2) {
+		 for (var key in object2) {
+		  object1[key] = object2[key];
+		 }
+		 return object1;
+		}
+							
 		function calculateResults(id){
 			loggerPool[id].runtime = loggerPool[id].stopTime - loggerPool[id].startTime;
 		}
@@ -25,7 +48,9 @@ var perfLogger = function(){
 		}
 
 		function logToServer(id){
-			var params = "data=" + (JSON.stringify(loggerPool[id]));
+			
+			var params = "data=" + JSON.stringify(jsonConcat(loggerPool[id],TestResults.prototype));
+			console.log(params)
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", serverLogURL, true);
 			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -35,7 +60,7 @@ var perfLogger = function(){
 			  {
 			  if (xhr.readyState==4 && xhr.status==200)
 			    {
-			    	console.log(xhr.responseText);
+			    	//console.log(xhr.responseText);
 			    }
 			  };
 			xhr.send(params);	
@@ -54,9 +79,9 @@ var perfLogger = function(){
 			return debuginfo
 		}
 		
-	return {
+	return {		
 	startTimeLogging: function(id, descr,drawToPage,logToServer){
-		loggerPool[id] = {};
+		loggerPool[id] = new TestResults();
 		loggerPool[id].id = id;
 		loggerPool[id].startTime =  performance.now();
 		loggerPool[id].description = descr;
@@ -67,7 +92,7 @@ var perfLogger = function(){
 	stopTimeLogging: function(id){
 		loggerPool[id].stopTime =  performance.now();
 		calculateResults(id);
-		setResultsMetaData(id);
+		setResultsMetaData(id);		
 		if(loggerPool[id].drawtopage){
 			drawToDebugScreen(id);
 		}
@@ -75,7 +100,6 @@ var perfLogger = function(){
 			logToServer(id);
 		}
 	},
-	
 	
 	logBenchmark: function(id, timestoIterate, func, debug, log){
 		var timeSum = 0;
@@ -92,6 +116,29 @@ var perfLogger = function(){
 		if(log){
 				logToServer(id)
 		}
+	},
+	
+	//expose derived performance data
+	perceivedTime: function(){
+		return _pTime;
+	}, 
+	redirectTime: function(){
+		_redirTime;
+	}, 
+	cacheTime: function(){
+		return _cacheTime;
+	}, 
+	dnsLookupTime: function(){
+		return _dnsTime;
+	},
+	tcpConnectionTime: function(){
+		return _tcpTime;
+	},
+	roundTripTime: function(){
+		return _roundtripTime;
+	},
+	pageRenderTime: function(){
+		return _renderTime;
 	}
 }
 }();
